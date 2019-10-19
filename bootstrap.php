f0e361f51a99aa1bf4bacf51f7eb830d4f2caa6d
bearpress <info@bearpress.org>
2019-10-19 06:44:11 -0700

    Changed @license to GPL3+.

--- bootstrap.php
+++ bootstrap.php
@@ -14,7 +14,7 @@
  * Author:      Git Contributing Team
  * Author URI:  https://KnowTheCode.io
  * Text Domain: git-contributing
- * License:     GPL-2.0+
+ * License:     GPL-3+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
